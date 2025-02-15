3814ab90a42fbe3112637283e827425ce89abbab
Nayara Matos <93952655+NayMatos@users.noreply.github.com>
2025-02-15 08:45:17 -0300

    Primeiro Commit

---
+++ listas/lista1/src/main/java/com/hackerrank/Ex2.java
@@ -0,0 +1,19 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+public class Ex2 {
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = scan.nextInt();
+
+    scan.close();
+    
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+
+}
+}
